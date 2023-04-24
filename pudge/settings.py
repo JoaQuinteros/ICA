194937ca9ee227a613e8bab414d55a2ad100e79e
Leo Monti <leomontigatti@gmail.com>
2023-04-24 12:59:14 -0300

    Added allowed hosts por production

--- pudge/settings.py
+++ pudge/settings.py
@@ -9,7 +9,9 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")]
+)
 
 INSTALLED_APPS = [
     "django.contrib.admin",
